df52ddb77edc50909a86bc68e1ccbfbf68bb927a
Sharjeel Ahmed <sharjeelahmed2404@gmail.com>
2024-10-26 21:04:16 +0500

    Initial commit of React Native app

---
+++ tempData.js
@@ -0,0 +1,117 @@
+const tempData = [
+  {
+    id: 1,
+    name: "Plan a Trip",
+    color: "#24A6D9",
+    todos: [
+      {
+        id: 1,
+        title: "Book Flight",
+        completed: true,
+      },
+      {
+        id: 2,
+        title: "Passport Check",
+        completed: true,
+      },
+      {
+        id: 3,
+        title: "Reserve Hotel Room",
+        completed: true,
+      },
+      {
+        id: 4,
+        title: "Pack Luggage",
+        completed: true,
+      },
+      {
+        id: 5,
+        title: "Buy Travel Insurance",
+        completed: true,
+      },
+    ],
+  },
+  {
+    id: 2,
+    name: "Grocery Shopping",
+    color: "#FF6347",
+    todos: [
+      {
+        id: 1,
+        title: "Buy Milk",
+        completed: true,
+      },
+      {
+        id: 2,
+        title: "Get Bread",
+        completed: true,
+      },
+      {
+        id: 3,
+        title: "Purchase Vegetables",
+        completed: false,
+      },
+      {
+        id: 4,
+        title: "Check for Spices",
+        completed: true,
+      },
+    ],
+  },
+  {
+    id: 3,
+    name: "Work on Project",
+    color: "#8E44AD",
+    todos: [
+      {
+        id: 1,
+        title: "Complete Homepage Design",
+        completed: true,
+      },
+      {
+        id: 2,
+        title: "Debug Errors",
+        completed: false,
+      },
+      {
+        id: 3,
+        title: "Submit Project Report",
+        completed: false,
+      },
+      {
+        id: 4,
+        title: "Review Code",
+        completed: false,
+      },
+    ],
+  },
+  {
+    id: 4,
+    name: "Daily Routine",
+    color: "#2ECC71",
+    todos: [
+      {
+        id: 1,
+        title: "Morning Exercise",
+        completed: true,
+      },
+      {
+        id: 2,
+        title: "Breakfast",
+        completed: false,
+      },
+      {
+        id: 3,
+        title: "Work/Study",
+        completed: false,
+      },
+      {
+        id: 4,
+        title: "Evening Walk",
+        completed: true,
+      },
+    ],
+  },
+];
+
+export default tempData;
